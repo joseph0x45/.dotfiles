4776fd0ad2a590962801324d8baf11eb105f6ce5
TheWisePigeon <zozozozeph@gmail.com>
2023-03-15 10:08:24 +0000

    sync wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,10 @@
 local wezterm = require 'wezterm'
 
 return {
-  
+  font_size = 10.5,
+  default_prog={'C:\\Program Files\\Git\\bin\\bash.exe', '--login'},
+  window_background_opacity= .85,
+  font = wezterm.font "JetBrainsMono Nerd Font Mono",
+  color_scheme = "tokyonight",
+  default_cwd = "D:\\"
 }
