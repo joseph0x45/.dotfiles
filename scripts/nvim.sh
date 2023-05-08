30eabd127e1fb8afbd97f248b13d12d93f324a67
TheWisePigeon <zozozozeph@gmail.com>
2023-05-08 23:47:11 +0000

    fix: config path

--- scripts/nvim.sh
+++ scripts/nvim.sh
@@ -1,4 +1,6 @@
 curl -o nvim.zip https://dotfiler.onrender.com/get_nvim
 mkdir -p ~/.config/nvim
 unzip -o nvim.zip -d  ~/.config/nvim
+mv ~/.config/nvim/after ~/.config/nvim/lua ~/.config/nvim/init.lua ~/.config
+rm -rf ~/.config/nvim
 rm nvim.zip
