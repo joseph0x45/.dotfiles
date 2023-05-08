49640bb12d645ff74ff3d08d32fba4bc9d068802
TheWisePigeon <zozozozeph@gmail.com>
2023-05-08 23:26:19 +0000

    fix: main file name

--- dotfiler.ts
+++ dotfiler.ts
@@ -5,7 +5,7 @@ const server = Bun.serve({
         if(req_url.pathname==="/nvim"){
             return new Response(Bun.file("./scripts/nvim.sh"))
         }
-	if(req_url.pathname==="/get_nvim){
+	if(req_url.pathname==="/get_nvim"){
     		return new Response(Bun.file("./assets/nvim.zip"))
     	}
         return new Response("")
