b1c29ae43b9e2dcd76a57be6af6ce4c38b716698
TheWisePigeon <zozozozeph@gmail.com>
2023-06-14 13:06:50 +0000

    minor updates

--- tmx.sh
+++ tmx.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
 
-echo "Suh"
+arg1=$1
+
+if [[ "$arg1" == "ls" ]]; then
+    tmux ls
+fi
+
+if [[ "$arg1" == "new" ]]; then
+    arg2=$2
+    if [ -z "$arg2" ]; then
+        echo "You have to specify the session name"
+        exit
+    fi
+    tmux new-session -s "$arg2"
+fi
+
+if [[ "$arg1" == "link" ]]; then
+    arg2=$2
+    if [ -z "$arg2" ]; then
+        echo "You have to specify the session name"
+        exit
+    fi
+    tmux attach-session -t "$arg2"
+fi
+
+if [[ "$arg1" == "resume" ]]; then
+    tmux run-shell "tmux-resurrect restore"
+fi
