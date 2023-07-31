6e1e0c997bbb59727d3d27e38c773af2761a98ce
TheWisePigeon <zozozozeph@gmail.com>
2023-07-31 09:02:58 +0000

    updates

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,1 @@
 require("pigeon")
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = "html",
-	command = "setlocal shiftwidth=2 tabstop=2"
-})
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = "svelte",
-	command = "setlocal shiftwidth=2 tabstop=2"
-})
