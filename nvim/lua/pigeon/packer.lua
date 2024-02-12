235de45d91e2429891ddee428eb117408228618c
TheWisePigeon <zozozozeph@gmail.com>
2024-02-12 14:05:54 +0000

    add discord presence

--- nvim/lua/pigeon/packer.lua
+++ nvim/lua/pigeon/packer.lua
@@ -1,55 +1,41 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
   use 'wbthomason/packer.nvim'
   use {
     'nvim-telescope/telescope.nvim', tag = '0.1.3',
-    -- or                            , branch = '0.1.x',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
-
-  -- use({
-    -- 	'rose-pine/neovim',
-    -- 	as = 'rose-pine',
-    -- 	config = function()
-      -- 		vim.cmd('colorscheme rose-pine')
-      -- 	end
-      -- })
-      use("folke/tokyonight.nvim")
-      use("theprimeagen/harpoon")
-      use("m4xshen/autoclose.nvim")
-      use {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v1.x',
-        requires = {
-          -- LSP Support
-          {'neovim/nvim-lspconfig'},
-          {'williamboman/mason.nvim'},
-          {'williamboman/mason-lspconfig.nvim'},
-
-          -- Autocompletion
-          {'hrsh7th/nvim-cmp'},
-          {'hrsh7th/cmp-buffer'},
-          {'hrsh7th/cmp-path'},
-          {'saadparwaiz1/cmp_luasnip'},
-          {'hrsh7th/cmp-nvim-lsp'},
-          {'hrsh7th/cmp-nvim-lua'},
-
-          -- Snippets
-          {'L3MON4D3/LuaSnip'},
-          {'rafamadriz/friendly-snippets'},
-        }
-      }
-      use {
-        'nvim-treesitter/nvim-treesitter',
-        run = function()
-          local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-          ts_update()
-        end,}
-        use("terrortylor/nvim-comment")
-        use "lukas-reineke/indent-blankline.nvim"
-        use({ 'toppair/peek.nvim', run = 'deno task --quiet build:fast' })
-        use({ 'tpope/vim-fugitive' })
-        use({ 'windwp/nvim-ts-autotag' })
-      end)
+  use("folke/tokyonight.nvim")
+  use("theprimeagen/harpoon")
+  use("m4xshen/autoclose.nvim")
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v1.x',
+    requires = {
+      {'neovim/nvim-lspconfig'},
+      {'williamboman/mason.nvim'},
+      {'williamboman/mason-lspconfig.nvim'},
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-buffer'},
+      {'hrsh7th/cmp-path'},
+      {'saadparwaiz1/cmp_luasnip'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'hrsh7th/cmp-nvim-lua'},
+      {'L3MON4D3/LuaSnip'},
+      {'rafamadriz/friendly-snippets'},
+    }
+  }
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = function()
+      local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+      ts_update()
+    end,}
+    use("terrortylor/nvim-comment")
+    use "lukas-reineke/indent-blankline.nvim"
+    use({ 'toppair/peek.nvim', run = 'deno task --quiet build:fast' })
+    use({ 'tpope/vim-fugitive' })
+    use({ 'windwp/nvim-ts-autotag' })
+    use 'andweeb/presence.nvim'
+  end)
