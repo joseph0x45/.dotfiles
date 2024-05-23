af62e9468eb7afa444421f3f5c76000034357166
TheWisePigeon <zozozozeph@gmail.com>
2024-05-23 15:43:39 +0000

    updated install script

--- install.sh
+++ install.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
-
-echo "REJOICE! You are installing TheWisePigeon's dotfiles \n"
-echo "Make sure you have Neovim and Git installed before proceeding"
-
-# Check if Git is installed
-if which git >/dev/null; then
-  echo "Git is installed."
-else
-  echo "Git is not installed. Aborting installation"
-  exit
-fi
-
-# configure tmux
-# First check for tmux existence on user system
-# Then create symlink of config from the dotfiles to ~/.config/tmux/tmux.conf
-# Install tmux plugin manager
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-# Run tmux and C-b I to install packages
+sudo ln -s /home/$USER/.dotfiles/i3 ~/.config/i3
+sudo ln -s /home/$USER/.dotfiles/tmux ~/.config/tmux
+sudo ln -s /home/$USER/.dotfiles/nvim ~/.config/nvim
+sudo ln -s /home/$USER/.dotfiles/alacritty ~/.config/alacritty
+tmux source ~/.config/tmux/tmux.conf
