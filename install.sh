b5bcaa09081563ee695edb677418558ff03a0770
TheWisePigeon <zozozozeph@gmail.com>
2024-05-27 20:59:13 +0000

    add git config commands to install script

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ sudo ln -s /home/$USER/.dotfiles/tmux ~/.config/tmux
 sudo ln -s /home/$USER/.dotfiles/nvim ~/.config/nvim
 sudo ln -s /home/$USER/.dotfiles/alacritty ~/.config/alacritty
 tmux source ~/.config/tmux/tmux.conf
+git config --global init.defaultBranch main
+git config --global --add --bool push.autoSetupRemote true
