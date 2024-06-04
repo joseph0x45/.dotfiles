9a4edc9a7c3f76bd2ab7e963eb7eaf10b3c0fe41
TheWisePigeon <zozozozeph@gmail.com>
2024-06-04 06:36:29 +0000

    remove gleam config and update harpoon maps

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -4,8 +4,8 @@ local ui = require("harpoon.ui")
 vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set("n", "<C-h>", ui.toggle_quick_menu)
 
-vim.keymap.set("n", "<C-q>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-w>", function() ui.nav_file(2) end, {noremap=true})
-vim.keymap.set("n", "<C-e>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<C-r>", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<leader>q", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<leader>w", function() ui.nav_file(2) end, {noremap=true})
+vim.keymap.set("n", "<leader>e", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<leader>r", function() ui.nav_file(4) end)
 
