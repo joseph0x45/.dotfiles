1a8898ab3b6d5ae60904997920be6ccb0cc4de9e
joseph0x45 <zozozozeph@gmail.com>
2024-08-08 10:56:32 +0000

    add scripts for enabling and disabling ipv6

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@ sudo ln -s /home/$USER/.dotfiles/i3 ~/.config/i3
 sudo ln -s /home/$USER/.dotfiles/tmux ~/.config/tmux
 sudo ln -s /home/$USER/.dotfiles/nvim ~/.config/nvim
 sudo ln -s /home/$USER/.dotfiles/alacritty ~/.config/alacritty
+sudo ln -s /home/$USER/.dotfiles/scripts/enable-ipv6.sh /usr/local/bin/enable-ipv6
+sudo ln -s /home/$USER/.dotfiles/scripts/disable-ipv6.sh /usr/local/bin/disable-ipv6
 tmux source ~/.config/tmux/tmux.conf
 git config --global init.defaultBranch main
 git config --global --add --bool push.autoSetupRemote true
