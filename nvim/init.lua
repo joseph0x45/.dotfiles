1f61d1fa020590d8ff23b47883b5006bb7d3a54c
joseph0x45 <zozozozeph@gmail.com>
2025-01-28 23:04:43 +0000

    tidy up neovim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,87 @@
-require("pigeon")
+-- keymaps
+print("New config yayyy")
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+vim.keymap.set("i", "<C-c>", "<Esc>")
+vim.keymap.set("n", "<C-s>", ":w<CR>")
+vim.keymap.set("i", "<C-s>", "<Esc>:w<CR>")
+vim.keymap.set("v", "<C-s>", "<Esc>:w<CR>")
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+vim.keymap.set("n", "<A-w>l", ":wincmd l<CR>")
+vim.keymap.set("n", "<A-w>k", ":wincmd k<CR>")
+vim.keymap.set("n", "<A-w>j", ":wincmd j<CR>")
+vim.keymap.set("n", "<A-w>h", ":wincmd h<CR>")
+vim.keymap.set("n", "<leader><leader>x", ":source ~/.dotfiles/nvim/init.lua<CR>")
+vim.keymap.set("n", "<leader><leader>", function()
+  vim.cmd("so")
+end)
+vim.cmd("colorscheme gruvbox")
+
+-- utility config
+vim.opt.guicursor = ""
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.termguicolors = true
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+vim.opt.updatetime = 50
+
+-- packer
+vim.cmd [[packadd packer.nvim]]
+require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+  use { 'nvim-telescope/telescope.nvim', tag = '0.1.3',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  use("theprimeagen/harpoon")
+  use("m4xshen/autoclose.nvim")
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v1.x',
+    requires = {
+      {'neovim/nvim-lspconfig'},
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-buffer'},
+      {'hrsh7th/cmp-path'},
+      {'saadparwaiz1/cmp_luasnip'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'hrsh7th/cmp-nvim-lua'},
+      {'L3MON4D3/LuaSnip'},
+      {'rafamadriz/friendly-snippets'},
+    }
+  }
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = function()
+      local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+      ts_update()
+    end,
+  }
+    use("terrortylor/nvim-comment")
+    use "lukas-reineke/indent-blankline.nvim"
+    use 'andweeb/presence.nvim'
+    use "joseph0x45/md_todo"
+    use "joseph0x45/arduinoo"
+    use "AbdelrahmanDwedar/awesome-nvim-colorschemes"
+  end)
+
+require("plugins.config")
