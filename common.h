e3cd3067d3585e80cc9280035655e3d54ff999f1
joseph0x45 <zozozozeph@gmail.com>
2025-06-25 23:12:43 +0000

    add common.h

---
+++ common.h
@@ -0,0 +1,28 @@
+#ifndef COMMON_H
+#define COMMON_H
+#define _POSIX_C_SOURCE 200809L
+#include <stdio.h>
+#include <string.h>
+
+int run_command(const char *cmd, char *output, size_t maxlen) {
+  FILE *fp;
+  char buffer[128];
+  if (!output || maxlen == 0) {
+    return -1;
+  }
+  output[0] = '\0';
+  fp = popen(cmd, "r");
+  if (!fp) {
+    return -1;
+  }
+  while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+    size_t space_left = maxlen - strlen(output) - 1;
+    if (space_left == 0) {
+      break;
+    }
+    strncat(output, buffer, space_left);
+  }
+  pclose(fp);
+  return 0;
+}
+#endif
