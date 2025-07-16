66031aa7c9863872fb0865803b56f583e3db5534
joseph0x45 <zozozozeph@gmail.com>
2025-07-16 23:05:26 +0000

    update neovim lsp config

--- nvim/after/plugin/lsp_config.lua
+++ nvim/after/plugin/lsp_config.lua
@@ -1,93 +1,69 @@
+local lsp = require("lsp-zero")
 local lspconfig = require("lspconfig")
-local cmp = require("cmp")
-local luasnip = require("luasnip")
-local lspconfig = require("lspconfig")
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
-vim = vim or {}
 
-local on_attach = function(client, bufnr)
-  local opts = { buffer = bufnr, remap = false }
-  vim.keymap.set("n", "gd", function()
-    vim.lsp.buf.definition()
-  end, opts)
+lsp.preset("recommended")
 
-  vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-  vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-  vim.keymap.set("n", "<leader>rr", function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+lsp.ensure_installed({
+})
+
+lsp.nvim_workspace()
+
+local servers = { }
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+  })
 end
+vim.filetype.add({ extension = { templ = "templ" } })
+lspconfig.html.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = { "html", "templ" },
+})
+
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+  ["<C-Space>"] = cmp.mapping.complete(),
+})
+
+cmp_mappings['<Tab>'] = nil
+cmp_mappings['<S-Tab>'] = nil
+
+lsp.setup_nvim_cmp({
+  mapping = cmp_mappings
+})
+
+lsp.set_preferences({
+    suggest_lsp_servers = false,
+    sign_icons = {
+        error = 'E',
+        warn = 'W',
+        hint = 'H',
+        info = 'I'
+    }
+})
+
+lsp.on_attach(function(client, bufnr)
+  local opts = {buffer = bufnr, remap = false}
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+end)
+
+lsp.setup()
 
 vim.diagnostic.config({
-  virtual_text = true,  -- inline messages
-  signs = true,         -- gutter signs
-  underline = true,     -- underline problems
-  update_in_insert = false, -- don't show new diagnostics while typing
-  severity_sort = true  -- show more severe problems first
+    virtual_text = true
 })
-
-cmp.setup({
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body) -- For `luasnip` users
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ["<C-Space>"] = cmp.mapping.complete(), -- Trigger completion
-    ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Confirm completion
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-  }),
-  sources = cmp.config.sources({
-    { name = "nvim_lsp" },
-    { name = "luasnip" },
-    { name = "buffer" },
-    { name = "path" },
-  }),
-})
-
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-
--- Language Server Configurations
-lspconfig.clangd.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  cmd = { "clangd" }
-}
-
-lspconfig.pylsp.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-}
-
-lspconfig.gopls.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-}
-
-lspconfig.html.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-}
-
-lspconfig.emmet_language_server.setup({})
-lspconfig.ts_ls.setup({})
--- nvim/lua/config/packer.lua
+++ nvim/lua/config/packer.lua
@@ -5,22 +5,24 @@ return require('packer').startup(function(use)
   use { 'nvim-telescope/telescope.nvim', tag = '0.1.3',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
-  use "theprimeagen/harpoon"
-  use "m4xshen/autoclose.nvim"
-  use "neovim/nvim-lspconfig"
-  use({
-    "L3MON4D3/LuaSnip",
-    tag = "v2.*",
-  })
+  use("theprimeagen/harpoon")
+  use("m4xshen/autoclose.nvim")
   use {
-    "hrsh7th/nvim-cmp",
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v1.x',
     requires = {
-      "hrsh7th/cmp-nvim-lsp",    -- LSP completion source
-      "hrsh7th/cmp-buffer",      -- Buffer completions
-      "hrsh7th/cmp-path",        -- Path completions
-      "hrsh7th/cmp-cmdline",     -- Command-line completions
-      "saadparwaiz1/cmp_luasnip" -- Snippet completions
-    },
+      {'neovim/nvim-lspconfig'},
+      {'williamboman/mason.nvim'},
+      {'williamboman/mason-lspconfig.nvim'},
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-buffer'},
+      {'hrsh7th/cmp-path'},
+      {'saadparwaiz1/cmp_luasnip'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'hrsh7th/cmp-nvim-lua'},
+      {'L3MON4D3/LuaSnip'},
+      {'rafamadriz/friendly-snippets'},
+    }
   }
   use {
     'nvim-treesitter/nvim-treesitter',
@@ -28,9 +30,10 @@ return require('packer').startup(function(use)
       local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
       ts_update()
     end,}
-    use "terrortylor/nvim-comment"
+    use("terrortylor/nvim-comment")
     use "lukas-reineke/indent-blankline.nvim"
+    use 'andweeb/presence.nvim'
     use "joseph0x45/md_todo"
     use "joseph0x45/arduinoo"
     use "AbdelrahmanDwedar/awesome-nvim-colorschemes"
- end)
+  end)
