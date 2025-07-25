92ada2a58943c67de15aa221f59ba1501303edc0
joseph0x45 <zozozozeph@gmail.com>
2025-07-25 09:05:37 +0000

    remove discord rich presence

--- nvim/lua/config/packer.lua
+++ nvim/lua/config/packer.lua
@@ -32,6 +32,5 @@ return require('packer').startup(function(use)
     end, }
   use("terrortylor/nvim-comment")
   use "lukas-reineke/indent-blankline.nvim"
-  use 'andweeb/presence.nvim'
   use "joseph0x45/md_todo"
 end)
