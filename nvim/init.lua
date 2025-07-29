07e85f6c51d141261eb22421c3c6980ac60b2d8d
joseph0x45 <zozozozeph@gmail.com>
2025-07-29 23:45:25 +0000

    update nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,12 @@
 require("config")
 require("toodoo")
+
+vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
+  pattern = "*.robot",
+  callback = function()
+    vim.opt_local.expandtab = true
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.softtabstop = 4
+  end,
+})
