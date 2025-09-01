f630c83b03f97fe4fab96d62c71d681030acdbf7
joseph0x45 <zozozozeph@gmail.com>
2025-09-01 09:20:14 +0000

    update script for setting refresh rate

--- utility-scripts/set_refresh_rate.sh
+++ utility-scripts/set_refresh_rate.sh
@@ -1,3 +1,14 @@
-#!/usr/bin/env sh
-$monitor = $1
-xrandr --output HDMI-$monitor --mode 1920x1080 --rate 180
+#!/usr/bin/env bash
+
+# Usage: ./set_refresh_rate.sh 2
+# This will run: xrandr --output HDMI-2 --mode 1920x1080 --rate 180
+
+MONITOR_NUM=$1
+
+if [ -z "$MONITOR_NUM" ]; then
+    echo "Usage: $0 <monitor_number>"
+    exit 1
+fi
+
+xrandr --output "HDMI-$MONITOR_NUM" --mode 1920x1080 --rate 180
+
