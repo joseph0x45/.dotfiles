3d565ae352454923b295d1c458cc22581e7c6e46
joseph0x45 <zozozozeph@gmail.com>
2025-12-25 07:29:41 +0000

    add treesitter submodule

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,3 +4,12 @@ require("autocmds")
 require("autoclose").setup()
 require("ibl").setup()
 require("telescope")
+require("nvim-treesitter.configs").setup {
+  ensure_installed = {},
+  sync_install = false,
+  auto_install = false,
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false
+  }
+}
