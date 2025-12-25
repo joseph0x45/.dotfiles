350921dbb27f7a91bff4359becec9a1543476208
joseph0x45 <zozozozeph@gmail.com>
2025-12-25 06:27:42 +0000

    add lua folder

---
+++ nvim/lua/autocmds.lua
@@ -0,0 +1,13 @@
+vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
+  pattern = "*.robot",
+  callback = function()
+    vim.opt_local.expandtab = true
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.softtabstop = 4
+  end,
+})
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { '*.go', '*.svelte' },
+  callback = function() vim.treesitter.start() end,
+})
