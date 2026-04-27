61ca878b2613da9424bec8ce7e05651ad7ba911c
joseph0x45 <zozozozeph@gmail.com>
2026-04-27 15:18:19 +0000

    add gen.lua

---
+++ nvim/lua/gen.lua
@@ -0,0 +1,39 @@
+vim.api.nvim_create_user_command("GenTestFlow", function(opts)
+    local name = opts.args
+
+    local property = name
+    local vars_key = name
+    local data_test = "grund_der_antragsstellung_" .. name
+
+    local output = table.concat({
+        "@property",
+        "def " .. property .. "(self) -> bool:",
+        "    return self.vars.setdefault(",
+        '        "' .. vars_key .. '",',
+        "        True,",
+        "        False,",
+        "        aw_utils.aw_random_bool(self)",
+        "    )",
+        "",
+        "def answer_" .. name .. "(self) -> bool:",
+        "    suffix = aw_utils.bool_to_english(self." .. property .. ")",
+        '    data_test = f"' .. data_test .. '-{suffix}"',
+        "    locator = f\"label[data-test='{data_test}'] span\"",
+        "",
+        "    aw_utils.aw_click_button_sr(",
+        "        self,",
+        "        locator,",
+        '        description="' .. name .. '"',
+        "    )",
+        "    return self." .. property,
+        "",
+        "def step_" .. name .. "(self):",
+        "    self.answer_" .. name .. "()",
+        "    if self." .. property .. ":",
+        "        pass",
+        "    else:",
+        "        pass",
+    }, "\n")
+
+    vim.api.nvim_put(vim.split(output, "\n"), "l", true, true)
+end, { nargs = 1 })
